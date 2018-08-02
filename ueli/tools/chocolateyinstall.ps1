011f804cccf2b8f58e8fed113d475f49364faa93
Tim Biermann <tim_biermann@web.de>
2018-08-02 10:12:45 +0200

    initial commit of ueli

---
+++ ueli/tools/chocolateyinstall.ps1
@@ -0,0 +1,40 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v5.0.0/ueli-setup-5.0.0.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'ueli*'
+
+  checksum      = '29F7A22CB8FE4ED51CD546544864787512B556DB12F9543950D0B9C26F74AADF'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
