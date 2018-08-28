f5dfcd85634b03b8f9dbd7b3540f5f5b8717206b
TimB87 <tim_biermann@web.de>
2018-08-28 16:11:13 +0200

    Update ueli to 5.1.0

--- ueli/tools/chocolateyinstall.ps1
+++ ueli/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v5.0.0/ueli-setup-5.0.0.exe'
+$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v5.1.0/ueli-setup-5.1.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'ueli*'
 
-  checksum      = '29F7A22CB8FE4ED51CD546544864787512B556DB12F9543950D0B9C26F74AADF'
+  checksum      = '5065F91ABC341315C3F4D8C6F74F91D66C759E93B54AE2D3E02D7ED22517A862'
   checksumType  = 'sha256'
 
   silentArgs   = '/S'
