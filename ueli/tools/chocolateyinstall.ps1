1f8cdc60ffb88e6b8b05aebf1194b66ca1c2478c
TimB87 <tim_biermann@web.de>
2018-11-30 16:14:46 +0100

    bump ueli to 7.0.0

--- ueli/tools/chocolateyinstall.ps1
+++ ueli/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v6.1.4/ueli-setup-6.1.4.exe'
+$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v7.0.0/ueli-setup-7.0.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'ueli*'
 
-  checksum      = '521008500A94606DDADAE3771839F25E53A927304EAFB4B063A706BB43D6857B'
+  checksum      = '372CE2F009F0B127255451286E8628AF9CBB3C39C0F68EEF5087FF159FFC0192'
   checksumType  = 'sha256'
 
   silentArgs   = '/S'
