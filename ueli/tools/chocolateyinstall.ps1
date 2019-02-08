ede2959ae58c3bd441314b1795b3a306f4b7e540
TimB87 <tim_biermann@web.de>
2019-02-08 20:15:11 +0100

    bump ueli to 7.2.5

--- ueli/tools/chocolateyinstall.ps1
+++ ueli/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v7.2.4/ueli-setup-7.2.4.exe'
+$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v7.2.5/ueli-setup-7.2.5.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'ueli*'
 
-  checksum      = 'FEF969E0A8EEDAA6EE456CFDC9DA8FFA84EFDF2E366414F3B1877ACCC1A3861B'
+  checksum      = '2E1343219F6FCEA261B510D1B76A050E538983E6E026F77680A88063437D4709'
   checksumType  = 'sha256'
 
   silentArgs   = '/S'
