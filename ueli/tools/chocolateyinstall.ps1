8d3b0ab9f8555c6970b32ff308d710e7cc419e45
TimB87 <tbier@posteo.de>
2019-09-04 13:40:22 +0200

    ueli: 8.1.0 -> 8.1.1

--- ueli/tools/chocolateyinstall.ps1
+++ ueli/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v8.1.0/ueli-Setup-8.1.0.exe'
+$url        = 'https://github.com/oliverschwendener/ueli/releases/download/v8.1.1/ueli-Setup-8.1.1.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'ueli*'
 
-  checksum      = 'FFBB3E4042F0D9859119F2082B868EDE8254889DD0B02C071A4523AC3F8618A9'
+  checksum      = 'DD7E826328F9FE984F2E277FA9605EF90F45836E8B6E14C040C4FA219B72B4A4'
   checksumType  = 'sha256'
 
   silentArgs   = '/S'
